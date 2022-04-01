5118546072fc6bafba45e1f0718963c8fe1fcb7e
Anthony Badea <badeaa3@gmail.com>
2022-04-01 13:02:47 +0200

    Add .exe and .root to gitignore and cleanup formatting

--- src/Thrust.cxx
+++ src/Thrust.cxx
@@ -9,55 +9,55 @@
 
 // c++ code
 #include <vector>
-#include <iostream> 
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 
-	// parse input
-	std::string inFileName = "/Users/anthonybadea/Documents/ALEPH/ALEPH/LEP1Data1992_recons_aftercut-MERGED.root";
-	std::string outFileName = "thrust.root";
+  // parse input
+  std::string inFileName = "/Users/anthonybadea/Documents/ALEPH/ALEPH/LEP1Data1992_recons_aftercut-MERGED.root";
+  std::string outFileName = "thrust.root";
 
-	// load input data
-	std::unique_ptr<TFile> f (new TFile(inFileName.c_str(), "READ"));
-	std::unique_ptr<TTree> t ((TTree*) f->Get("t"));
+  // load input data
+  std::unique_ptr<TFile> f (new TFile(inFileName.c_str(), "READ"));
+  std::unique_ptr<TTree> t ((TTree*) f->Get("t"));
 
-	// declare variables
-	int maxPart = 500;
-	int nParticle;
-	float px[maxPart];
-	float py[maxPart];
-	float pz[maxPart];
+  // declare variables
+  int maxPart = 500;
+  int nParticle;
+  float px[maxPart];
+  float py[maxPart];
+  float pz[maxPart];
 
-	// link memory
-	t->SetBranchAddress("nParticle", &nParticle);
-	t->SetBranchAddress("px", &px);
-	t->SetBranchAddress("py", &py);
-	t->SetBranchAddress("pz", &pz);
+  // link memory
+  t->SetBranchAddress("nParticle", &nParticle);
+  t->SetBranchAddress("px", &px);
+  t->SetBranchAddress("py", &py);
+  t->SetBranchAddress("pz", &pz);
 
-	// create output tree
-	std::unique_ptr<TFile> fout (new TFile(outFileName.c_str(), "RECREATE"));
-	std::unique_ptr<TTree> tout (new TTree("t",""));
-	float Thrust;
-	tout->Branch("Thrust", &Thrust, "Thrust/F");
+  // create output tree
+  std::unique_ptr<TFile> fout (new TFile(outFileName.c_str(), "RECREATE"));
+  std::unique_ptr<TTree> tout (new TTree("t", ""));
+  float Thrust;
+  tout->Branch("Thrust", &Thrust, "Thrust/F");
 
-	// variables used in loop
-	TVector3 thrust;
+  // variables used in loop
+  TVector3 thrust;
 
-	// loop over events
-	int nEvents = t->GetEntries();
-	for (int iE = 0; iE < nEvents; iE++ ) {
+  // loop over events
+  int nEvents = t->GetEntries();
+  for (int iE = 0; iE < nEvents; iE++ ) {
 
-		// use input data
-		t->GetEntry(iE);
-		thrust = getThrust(nParticle, px, py, pz, THRUST::OPTIMAL); //, false, false, pDataReader.weight);
+    // use input data
+    t->GetEntry(iE);
+    thrust = getThrust(nParticle, px, py, pz, THRUST::OPTIMAL); //, false, false, pDataReader.weight);
 
-		// fill output tree
-		Thrust = thrust.Mag();
-		tout->Fill();
-	}
+    // fill output tree
+    Thrust = thrust.Mag();
+    tout->Fill();
+  }
 
-	// write to output file
-	tout->Write();
+  // write to output file
+  tout->Write();
 
-	return 1;
+  return 1;
 }
